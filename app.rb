622e6e7823f27ea5e30030ab5723ca737708f4f3
Mohan Krishnan <mohangk@gmail.com>
2013-08-11 16:47:47 +0800

    Only guard editing routes

--- app.rb
+++ app.rb
@@ -5,13 +5,12 @@ Bundler.require
 
 require 'gollum/app'
 require 'digest/sha1'
- 
+
 class App < Precious::App
-  User = Struct.new(:name, :email, :password_hash, :can_write)
- 
-  before { authenticate! }
-  before /^\/(edit|create|delete|livepreview|revert)/ do authorize_write! ; end
- 
+  User = Struct.new(:name, :email, :password_hash)
+
+  before /^\/(edit|create|delete|rename|livepreview|revert)/ do  authenticate!; end
+
   helpers do
     def authenticate!
       @_auth ||=  Rack::Auth::Basic::Request.new(request.env)
@@ -24,15 +23,15 @@ class App < Precious::App
         throw(:halt, [401, "Not authorized\n"])
       end
     end
- 
+
     def authorize_write!
       throw(:halt, [403, "Forbidden\n"]) unless @user.can_write
     end
- 
+
     def users
       @_users ||= settings.authorized_users.map {|u| User.new(*u) }
     end
- 
+
     def detected_user(credentials)
       users.detect do |u|
         [u.email, u.password_hash] ==
@@ -40,7 +39,7 @@ class App < Precious::App
       end
     end
   end
- 
+
   def commit_message
     {
       :message => params[:message],
--- config.ru
+++ config.ru
@@ -9,5 +9,5 @@ require 'yaml'
 require 'app'
 App.set(:gollum_path, "~/apps/wiki")
 App.set(:authorized_users, YAML.load_file(File.expand_path('users.yml', __DIR__)))
-App.set(:wiki_options, {})
+App.set(:wiki_options, { is_bare: true, h1_title: true })
 run App
